12f483393cd5918f9d0acf18d7634fb5008c5d74
ArelyXl <andrycaceres2006@gmail.com>
2025-01-25 12:56:24 -0500

    Planos 3d y 2d escena 0_1

---
+++ ggj2025/Arte/Arte2D/Personajes/character_body_2d.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var SPEED = 200
+
+@onready var player = $"."
+@onready var animation = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if Global.isThreeD == true && Global.isTwoD == false:
+		self.queue_free()
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	velocity = input_dir * SPEED
+
+	# Determine animation based on movement direction
+	if input_dir.y < 0 || input_dir.y > 0 || input_dir.x > 0 || input_dir.x < 0:
+		animation.play("General_Sprite")
+	
+	# Stop animation when no movement
+	if input_dir == Vector2.ZERO:
+		# animation.stop()
+		animation.play("General_Sprite")
+
+	move_and_slide()
+
+
+func _on_portal_2d_body_entered(body: Node2D) -> void:
+	Global.isThreeD = true
+	Global.isTwoD = false
