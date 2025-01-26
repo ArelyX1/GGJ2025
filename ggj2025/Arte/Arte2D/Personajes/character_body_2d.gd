77960d4fe884160037c75e744095c3d6c1db164a
ArelyXl <andrycaceres2006@gmail.com>
2025-01-25 19:51:31 -0500

    models with viewports textures added

--- ggj2025/Arte/Arte2D/Personajes/character_body_2d.gd
+++ ggj2025/Arte/Arte2D/Personajes/character_body_2d.gd
@@ -2,30 +2,23 @@ extends CharacterBody2D
 
 var SPEED = 200
 
-@onready var player = $"."
+@onready var player = $".."
 @onready var animation = $AnimatedSprite2D
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Global.isTwoD == false && Global.isThreeD == true:
-		self.visible = false
+		self.queue_free()
+		
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity = input_dir * SPEED
+	if input_dir != Vector2.ZERO:
+		animation.play("General_Sprite")
 
-	# Determine animation based on movement direction
-	if input_dir.y < 0 || input_dir.y > 0 || input_dir.x > 0 || input_dir.x < 0:
-		animation.play("General_Sprite")
-	
-	# Stop animation when no movement
-	if input_dir == Vector2.ZERO:
-		# animation.stop()
-		animation.play("General_Sprite")
 
 	move_and_slide()
 
 
-func _on_portal_2d_body_entered(body: Node2D) -> void:
-	Global.isThreeD = true
-	Global.isTwoD = false
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
